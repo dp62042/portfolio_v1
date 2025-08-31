1f82c9bbc355b8ce84beb4e97d44a4aedd3f9929
dp62042 <dharmendra193728@gmail.com>
2025-08-31 15:40:45 +0530

    Add Next.js configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   experimental: {
-    typedRoutes: true
-  }
-};
-export default nextConfig;
+    typedRoutes: true,
+  },
+}
+export default nextConfig
